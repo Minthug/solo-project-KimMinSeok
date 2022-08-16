6900fab1c838d05a4453f4ae4eaf432f06cb81db
Minseok Kim <jkisimmortal4@gmail.com>
2022-08-16 14:39:48 +0900

    first commit

---
+++ Section4SoloProject/src/main/java/com/section4/Section4SoloProject/Section4SoloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.section4.Section4SoloProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section4SoloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section4SoloProjectApplication.class, args);
+	}
+
+}
---
+++ Section4SoloProject/src/test/java/com/section4/Section4SoloProject/Section4SoloProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section4.Section4SoloProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section4SoloProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
